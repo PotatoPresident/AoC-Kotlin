5f3902884ebcd7c5ac2722fa36c6e632d648f162
Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>
2023-12-08 22:20:54 -0800

    2023 Day 9 Cleanup

--- src/y23/Day9.kt
+++ src/y23/Day9.kt
@@ -4,45 +4,20 @@ import findLongs
 import puzzle
 
 fun main() = puzzle(2023, 9) {
-    val histories = inputLines.map { it.findLongs() }
+    val histories = inputLines.map { hs ->
+        generateSequence(hs.findLongs()) { prevList -> prevList.windowed(2) { (a, b) -> b - a } }
+            .takeWhile { it.any { num -> num != 0L } }.toList()
+    }
 
     submit {
-        histories.sumOf { list ->
-            val lists = mutableListOf<MutableList<Long>>()
-            lists.add(list.toMutableList())
-            var i = 0
-
-            while (lists.last().any { it != 0L }) {
-                val new = mutableListOf<Long>()
-                lists.add(new)
-                lists[i].windowed(2).forEach { longs ->
-                    new.add(longs[1] - longs[0])
-                }
-                if (new.isEmpty()) new.add(0)
-                i++
-            }
-
-            lists.sumOf { it.last() }
+        histories.sumOf { history ->
+            history.sumOf { it.last() }
         }
     }
 
     submit {
-        histories.sumOf { list ->
-            val lists = mutableListOf<MutableList<Long>>()
-            lists.add(list.toMutableList())
-            var i = 0
-
-            while (lists.last().any { it != 0L }) {
-                val new = mutableListOf<Long>()
-                lists.add(new)
-                lists[i].windowed(2).forEach { longs ->
-                    new.add(longs[1] - longs[0])
-                }
-                if (new.isEmpty()) new.add(0)
-                i++
-            }
-
-            lists.foldRight(0L) { l, acc -> l.first() - acc }
+        histories.sumOf { history ->
+            history.foldRight(0L) { l, acc -> l.first() - acc }
         }
     }
 }
