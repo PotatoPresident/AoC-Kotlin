f92b75874b1b293c616f96c79ff12ce8d296b989
Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>
2023-12-12 10:56:00 -0800

    2023 Day 12 - Slight cleanup?

--- src/y23/Day12.kt
+++ src/y23/Day12.kt
@@ -1,31 +1,34 @@
 package y23
 
 import findInts
-import log
 import puzzle
 
 fun main() = puzzle(2023, 12) {
     val rows = inputLines.map { it.split(' ') }.map { (a, b) -> a to b.findInts() }
 
-    fun generatePermutations(input: String, cont: List<Int>, i: Int = 0, j: Int = 0, cur: Int = 0, mem: MutableMap<Triple<Int, Int, Int>, Long> = mutableMapOf()): Long {
+    fun countPermutations(input: String, cont: List<Int>, i: Int = 0, j: Int = 0, cur: Int = 0, mem: MutableMap<Triple<Int, Int, Int>, Long> = mutableMapOf()): Long {
         val key = Triple(i, j, cur)
         if (key in mem) {
             return mem[key]!!
         }
 
-        if (i == input.length) {
+        if (i == input.length) { // Reached end, check if valid
             return if ((j == cont.size && cur == 0) || (j == cont.size - 1 && cont[j] == cur)) 1 else 0
         }
+
         val c = input[i]
         var total = 0L
-        if (cur == 0 && (c == '.' || c == '?')) {
-            total += generatePermutations(input, cont, i + 1, j, 0, mem) // Start of streak
-        } else if (cur > 0 && j < cont.size && cont[j] == cur && (c == '.' || c == '?')) {
-            total += generatePermutations(input, cont, i + 1, j + 1, 0, mem) // End of streak
+
+        if (c == '.' || c == '?') { // It's a . or it could be a .
+            if (cur == 0) { // No current streak
+                total += countPermutations(input, cont, i + 1, j, 0, mem) // Count permutations as if it was a .
+            } else if (cur > 0 && j < cont.size && cont[j] == cur) { // Ends valid streak
+                total += countPermutations(input, cont, i + 1, j + 1, 0, mem) // Count permutations looking for next streak
+            }
         }
 
-        if (c == '#' || c == '?') {
-            total += generatePermutations(input, cont, i + 1, j, cur + 1, mem) // Continue streak
+        if (c == '#' || c == '?') { // It's a # or it could be a #
+            total += countPermutations(input, cont, i + 1, j, cur + 1, mem) // Continue streak
         }
         mem[key] = total
         return total
@@ -33,20 +36,20 @@ fun main() = puzzle(2023, 12) {
 
     submit {
         rows.sumOf { (springs, cont) ->
-            generatePermutations(springs, cont).log()
+            countPermutations(springs, cont)
         }
     }
 
     submit {
         rows.sumOf { (springs, cont) ->
             var newSprings = springs
-            var newCont = cont
+            val newCont = cont.toMutableList()
             repeat(4) {
-                newSprings += "?" + springs
+                newSprings += "?$springs"
                 newCont += cont
             }
 
-            generatePermutations(newSprings, newCont)
+            countPermutations(newSprings, newCont)
         }
     }
 }
