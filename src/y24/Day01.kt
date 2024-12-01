c23632a2b23a4b9c5fc5fd7283d7c514e65ef29c
PotatoPresident <potatoboy9999@outlook.com>
2024-12-01 23:03:20 +0000

    Refactor day 1

--- src/y24/Day01.kt
+++ src/y24/Day01.kt
@@ -3,34 +3,17 @@ package y24
 import findLongs
 import puzzle
 import transpose
-import kotlin.math.abs
+import kotlin.math.absoluteValue
 
 fun main() = puzzle(2024, 1) {
-    val sides = inputLines.map { it.findLongs() }.transpose()
+    val (left, right) = inputLines.map { it.findLongs() }.transpose().map { it.sorted() }
 
     submit {
-        val left = sides[0].sorted()
-        val right = sides[1].sorted()
-        val n = left.size
-        var sum = 0L
-
-        for (i in 0 until n) {
-            val diff = abs(left[i] - right[i])
-            sum += diff
-        }
-
-        sum
+        left.zip(right).sumOf { (a, b) -> (b - a).absoluteValue }
     }
 
     submit {
-        val counts = sides[1].groupingBy { it }.eachCount()
-
-        var res = 0L
-        for (num in sides[0]) {
-            if (num in counts) {
-                res += num * counts[num]!!
-            }
-        }
-        res
+        val counts = right.groupingBy { it }.eachCount()
+        left.sumOf { it * (counts[it] ?: 0) }
     }
 }
