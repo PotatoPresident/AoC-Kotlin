0e3c9b62d5b45e3c904f1155881498aa528e567b
Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>
2024-12-18 22:23:10 -0800

    2024 Day 19 cleanup

--- src/y24/Day19.kt
+++ src/y24/Day19.kt
@@ -8,23 +8,17 @@ fun main() = puzzle(2024, 19) {
     val designs = inputLines.splitOnEmpty()[0][0].split(", ")
     val targets = inputLines.splitOnEmpty()[1]
 
-    submit {
-        @Memoize
-        fun canMake(target: String): Boolean {
-            if (target.isEmpty()) return true
-            return designs.filter { target.startsWith(it) }.any { canMake(target.drop(it.length)) }
-        }
-
-        targets.count { canMake(it) }
+    @Memoize
+    fun make(target: String): Long {
+        if (target.isEmpty()) return 1L
+        return designs.filter { target.startsWith(it) }.sumOf { make(target.drop(it.length)) }
     }
 
     submit {
-        @Memoize
-        fun make(target: String): Long {
-            if (target.isEmpty()) return 1L
-            return designs.filter { target.startsWith(it) }.sumOf { make(target.drop(it.length)) }
-        }
+        targets.count { make(it) > 0 }
+    }
 
+    submit {
         targets.sumOf { make(it) }
     }
 }
